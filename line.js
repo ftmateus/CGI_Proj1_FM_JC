447a35ad8afa5776b5b62083ac56a266b078bf8e
Francisco Mateus <francisco.mateus17@gmail.com>
2019-10-11 23:28:53 +0100

    cores funcionam

--- line.js
+++ line.js
@@ -5,14 +5,26 @@ var bufferlineId, bufferParticlesId;
 var startPos;
 var endPos;
 var isDrawing = false;
-var startPosAttrib, timeAttrib, exploSpeedAttrib, speedAttrib, firstExplosionAttrib;
+var startPosAttrib, timeAttrib, exploSpeedAttrib, explo2SpeedAttrib, speedAttrib, firstExplosionAttrib;
 var vPositionAttrib;
 var global_timeLoc;
 var global_time = 0;
 var numParticles = 0;
 var automaticLaunchSet = false;
+var colors = [
+    vec4( 1.0, 0.0, 0.0, 1.0),  // red
+    vec4( 1.0, 1.0, 0.0, 1.0),  // yellow
+    vec4( 0.0, 1.0, 0.0, 1.0),  // green
+    vec4( 0.0, 0.0, 1.0, 1.0),  // blue
+    vec4( 1.0, 0.0, 1.0, 1.0),  // magenta
+    vec4( 0.0, 1.0, 1.0, 1.0)   // cyan
+];
+var currentColor;
+var vColor_line;
+var vColor_particle;
 
 const NUM_PARTICLES = 65000;
+const COLOR_SIZE = 16;
 const FLOAT_SIZE = 4;
 const STARTPOS_SIZE = 2*FLOAT_SIZE,
 SPEED_SIZE = 2*FLOAT_SIZE,
@@ -20,7 +32,7 @@ EXPLOSPEED_SIZE = 2*FLOAT_SIZE;
 const TIME_SIZE = FLOAT_SIZE,
 FIRSTEXPLOTIME_SIZE = FLOAT_SIZE;
 const PARTICLE_STRIDE = 
-STARTPOS_SIZE + SPEED_SIZE + EXPLOSPEED_SIZE+ TIME_SIZE + FIRSTEXPLOTIME_SIZE;
+STARTPOS_SIZE + SPEED_SIZE + EXPLOSPEED_SIZE + EXPLOSPEED_SIZE + TIME_SIZE + FIRSTEXPLOTIME_SIZE + COLOR_SIZE;
 
 
 
@@ -53,12 +65,12 @@ function keyPress(ev)
     {
         automaticLaunchSet = automaticLaunchSet ? false : true;
     }
-    console.log(automaticLaunchSet);
 }
 
 function automaticLaunch()
 {
     var posX = Math.random() * (1 /*max*/  + 1 /*min*/) - 1 /*min*/;
+    randomColor();
     createMortarParticle([posX, -1], [posX, -0.3]);
 }
 
@@ -67,8 +79,9 @@ function initLineProgram()
     program_line = initShaders(gl, "vertex-shader-line", "fragment-shader");
     bufferlineId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferlineId);
-    gl.bufferData(gl.ARRAY_BUFFER, 16, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, 4*4 + 4*4 + 4*4, gl.STATIC_DRAW);
     vPositionAttrib = gl.getAttribLocation(program_line, "vPosition");
+    vColor_line = gl.getAttribLocation(program_line, "vColor");
 }
 
 function initParticlesProgram()
@@ -84,9 +97,14 @@ function initParticlesProgram()
     startPosAttrib = gl.getAttribLocation(program_particles, "startPos");
     speedAttrib = gl.getAttribLocation(program_particles, "speed");
     exploSpeedAttrib = gl.getAttribLocation(program_particles, "exploSpeed");
+    explo2SpeedAttrib = gl.getAttribLocation(program_particles, "explo2Speed");
     timeAttrib = gl.getAttribLocation(program_particles, "time");    
     firstExplosionAttrib = gl.getAttribLocation(program_particles, "firstExploTime");
+    vColor_particle = gl.getAttribLocation(program_particles, "vColor_particle");
+    //console.log(vColor_particle);
     global_timeLoc = gl.getUniformLocation(program_particles, "global_time");
+    
+
 }
 
 function getMousePos(canvas, ev) {
@@ -95,13 +113,20 @@ function getMousePos(canvas, ev) {
     return vec2(x,y);    
 }
 
+function randomColor()
+{
+    currentColor = vec4(colors[Math.ceil(Math.random()*(colors.length-1))]);
+}
+
 function mouseDown(ev) {
     isDrawing = true;
     startPos = getMousePos(canvas, ev);
     endPos = startPos;
+    randomColor();
     
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferlineId);
     gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten([startPos, endPos]));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 16, flatten([currentColor, currentColor]));
 }
 
 function mouseMove(ev) {
@@ -122,24 +147,39 @@ function createMortarParticle(startPos_i, endPos_i)
     //considerando que o intervalo de tempo e 1 nas 2 componentes
     var speedX = 5 *(endPos_i[0] - startPos_i[0]);
     var speedY = 7 *(endPos_i[1] - startPos_i[1]);
+    var explosion_time = speedY/10.0;    
+    var i = 0;
 
-    var explosion_time =  speedY/10.0;
-    
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferParticlesId);
     var num_explosion_particles = Math.ceil(Math.random() * (250 /*max*/ - 10 /*min*/) + 10 /*min*/);
     var thetaoffset = (2*Math.PI)/num_explosion_particles;
 
     for (var theta = 0 ; theta <= 2*Math.PI; theta += thetaoffset)
     {
-        // coordenadas polares
-        var exploSpeedX = Math.random() * Math.cos(theta);
-        var exploSpeedY = Math.random()* Math.sin(theta);
+        var exploSpeedX, exploSpeedY, explo2SpeedX, explo2SpeedY;
+        if(i == 0){
+            // coordenadas polares
+            exploSpeedX = 0.5 * Math.random() * Math.cos(theta);
+            exploSpeedY = 0.5 * Math.random() * Math.sin(theta);
+        }    
+        if(i == 2){
+            explo2SpeedX = 0.5 * exploSpeedX;
+            explo2SpeedY = 0.5 * exploSpeedY;
+            i = 0;
+        }
+        else{
+            explo2SpeedX = 0.5 * Math.random() * Math.cos(theta);
+            explo2SpeedY = 0.5 * Math.random() * Math.sin(theta);
+            i++;
+        }
 
-        var particle_data = new Float32Array([startPos_i[0], startPos_i[1], speedX, speedY, exploSpeedX, exploSpeedY, global_time, explosion_time]);
+        var particle_data = new Float32Array([startPos_i[0], startPos_i[1], speedX, speedY, exploSpeedX, exploSpeedY,
+             explo2SpeedX, explo2SpeedY, global_time, explosion_time, currentColor[0], currentColor[1], currentColor[2], currentColor[3] ]);
+        
         gl.bufferSubData(gl.ARRAY_BUFFER, (numParticles)*PARTICLE_STRIDE, particle_data);
 
         if (++numParticles >= NUM_PARTICLES)
-            numParticles = 0; //vai para o inicio do buffer, eliminando os antigos
+            numParticles = 0; //vai para o inicio do buffer, eliminando os antigos        
     }
 }
 
@@ -149,6 +189,8 @@ function drawLine()
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferlineId);  
     gl.vertexAttribPointer(vPositionAttrib, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPositionAttrib);
+    gl.vertexAttribPointer(vColor_line, 4, gl.FLOAT, false, 0, 16);
+    gl.enableVertexAttribArray(vColor_line);
     gl.drawArrays(gl.POINTS, 0, 1); 
     gl.drawArrays(gl.LINES, 0, 2); 
 }
@@ -167,13 +209,17 @@ function moveParticles()
     gl.vertexAttribPointer(exploSpeedAttrib, 2, gl.FLOAT, false, PARTICLE_STRIDE, STARTPOS_SIZE + SPEED_SIZE);
     gl.enableVertexAttribArray(exploSpeedAttrib);
 
-    gl.vertexAttribPointer(timeAttrib, 1, gl.FLOAT, false, PARTICLE_STRIDE,
-         STARTPOS_SIZE + SPEED_SIZE +EXPLOSPEED_SIZE);
+    gl.vertexAttribPointer(explo2SpeedAttrib, 2, gl.FLOAT, false, PARTICLE_STRIDE, STARTPOS_SIZE + SPEED_SIZE + SPEED_SIZE);
+    gl.enableVertexAttribArray(explo2SpeedAttrib);
+
+    gl.vertexAttribPointer(timeAttrib, 1, gl.FLOAT, false, PARTICLE_STRIDE, STARTPOS_SIZE + SPEED_SIZE + SPEED_SIZE + SPEED_SIZE);
     gl.enableVertexAttribArray(timeAttrib);
 
-    gl.vertexAttribPointer(firstExplosionAttrib, 1, gl.FLOAT, false, PARTICLE_STRIDE, 
-         STARTPOS_SIZE + SPEED_SIZE +EXPLOSPEED_SIZE + TIME_SIZE);   
+    gl.vertexAttribPointer(firstExplosionAttrib, 1, gl.FLOAT, false, PARTICLE_STRIDE, STARTPOS_SIZE + SPEED_SIZE + SPEED_SIZE + SPEED_SIZE + TIME_SIZE);
     gl.enableVertexAttribArray(firstExplosionAttrib);
+
+    gl.vertexAttribPointer(vColor_particle, 4, gl.FLOAT, false, PARTICLE_STRIDE, STARTPOS_SIZE + SPEED_SIZE + SPEED_SIZE + SPEED_SIZE + TIME_SIZE + TIME_SIZE);   
+    gl.enableVertexAttribArray(vColor_particle);
 
     global_time += 0.005; 
     gl.uniform1f(global_timeLoc, global_time);
